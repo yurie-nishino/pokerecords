bd8bc849bcae687cf9481671d65f769e14309684
yurie-nishino <nishinoyurie0314@gmail.com>
2020-08-09 17:57:03 +0900

    coment_edit
    コメントの修正

--- src/js/records/containers/Form.jsx
+++ src/js/records/containers/Form.jsx
@@ -3,23 +3,24 @@ import Form from "../component/form";
 
 class Forms extends React.Component {
   constructor(props) {
+    //初期値の設定
     super(props);
     this.state = {
       fileData: "",
     };
-    this.onFileSelect = this.onFileSelect.bind(this);
+    this.onFileSelect = this.onFileSelect.bind(this);//ファイル投稿フォームの初期値。bindは別のファイルで関数を使いたいから書いている。
   }
-  dispatchFileData(data) {
+  dispatchFileData(data) { // 送られてきたファイルの中身
     this.setState({
       fileData: data,
     });
   }
-  onFileSelect(file) {
+  onFileSelect(file) {  //ボタンが押された時に発火する関数。そのなかでファイルを読み込むクラスを使っている。
     const reader = new FileReader();
     reader.onload = () => {
-      this.dispatchFileData(reader.result);
+      this.dispatchFileData(reader.result);//読み込んだファイル内容を保存する関数に送っている
     };
-    reader.readAsDataURL(file.target.files[0]);
+    reader.readAsDataURL(file.target.files[0]);//疑問4 読み込んだファイルをURLで表示するように指示している
   }
 
   render() {
