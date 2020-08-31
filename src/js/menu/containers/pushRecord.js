3f34149caa9e6b28a695967bafb65df7a75f2674
yurie-nishino <nishinoyurie0314@gmail.com>
2020-08-31 18:30:46 +0900

    pushRecord_edit
    recordの画面遷移のため、Menu.jsxやReducerやContainarの見直し。

--- src/js/menu/containers/pushRecord.js
+++ src/js/menu/containers/pushRecord.js
@@ -7,13 +7,13 @@ function mapStateToProps(state) {
   return {
     isPushRecord: state.record.status.isPushed,
   }
-}
+}// isPushRecordは、recordReducerの処理を返す。stateの中のrecord（mainReducer）のstatus(状態)がisPushed（）のTrue or Falseの状態を返す
+//Menuでstateの結果を扱えるようにし、trueの際に＜Forms /＞へ画面遷移するようにしている
 
 function mapDispatchToProps(dispatch) {
     return {
-    // pushEnter: () => dispatch(ACTIONS.pushEnter()),
+    pushRecord: () => dispatch(ACTIONS.pushRecord()),
   };
-}
-
+}//Menuの中のpushRecordを使えるようにしている？必要ない？
 
 export default connect(mapStateToProps, mapDispatchToProps)(Menu);
