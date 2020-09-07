4cb5b9105a188b31b68c7c23e4d6cf68e6bfddde
ShiotaYoshiaki <shiota.yosiaki.0904@gmail.com>
2020-09-07 22:02:56 +0900

    styledの適用

--- src/js/home/component/home.jsx
+++ src/js/home/component/home.jsx
@@ -1,20 +1,26 @@
 import React from "react";
 import css from "./home.module.css";
 import Button from '@material-ui/core/Button';
+import { styled } from '@material-ui/core/styles';
+
+const MyButton = styled(Button)({
+  borderRadius: 30,
+});
 
 class Home extends React.Component {
   render() {
     const {
       pushEnter,
     } = this.props;
+    
     return (
       <React.Fragment>
         <div className = {css.Home_header}>
           <h1> 
             Pokemon <span>Records</span>
           </h1>
-          <Button variant="contained" color="primary" className={css.record_btn}>Sign Up</Button>
-          <Button variant="contained" color="primary" onClick={pushEnter} className={css.record_btn}>Sign In</Button>
+          <MyButton variant="contained" color="primary" className={css.record_btn}>Sign Up</MyButton>
+          <MyButton variant="contained" color="primary" onClick={pushEnter} className={css.record_btn}>Sign In</MyButton>
         </div>
       </React.Fragment>
     );
