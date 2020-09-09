d756212bf89af692841aa42e712559d67ce9cc9b
yurie-nishino <nishinoyurie0314@gmail.com>
2020-09-09 16:29:11 +0900

    buttun_costum

--- src/js/recordForm/photo/component/image.jsx
+++ src/js/recordForm/photo/component/image.jsx
@@ -2,26 +2,35 @@ import React from "react";
 // import css from "./record.module.css";
 import IconButton from "@material-ui/core/IconButton";
 import PhotoCamera from "@material-ui/icons/PhotoCamera";
+import { styled } from '@material-ui/core/styles';
+
+const PhotoIconButton = styled(IconButton)({
+  color: "#49a9d4",
+});
+
+// const ImagePhotoCamera = styled(PhotoCamera)({
+//   size: "30px",
+//   boxShadow: "0 3px 0 #bcd2fa",
+// });
 
 class Image extends React.Component {
   render() {
     return (
       <div>
-        <label>
-          <p>写真</p>
+        <p>写真</p>
+          <label>
           <input
             onChange={(file) => this.props.onFileSelect(file)}
             type="file"
             id="file-input"
             style={{ display: "none" }}
           />
-          <IconButton
-            color="primary"
+          <PhotoIconButton
             aria-label="upload picture"
             component="span"
           >
             <PhotoCamera />
-          </IconButton>
+          </PhotoIconButton>
         </label>
       </div>
     );
